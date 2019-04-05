9da56f5d00543ec086deaa74b9ac24509f9f32ac
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-04-05 09:25:13 +0200

    klik op instelling maakt div instellig aan

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,13 @@
 let instellingen_tag = document.getElementById('instellingen');
+let c = document.getElementsByClassName("instelling");
+
 
 instellingen_tag.addEventListener('click', () => {
-    //open een div
+    console.log("open instelligen")
+    let instelligen = document.createElement("div");
+    let instelligen_inhoud = document.createTextNode("Hallo wereld");
+    instelligen.appendChild(instelligen_inhoud);
+
+    let node_instellignen = document.getElementById("instellingnode");
+    node_instellignen.appendChild(instelligen);
 })
