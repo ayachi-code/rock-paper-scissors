185a00ac1ec0cf3e28860d97b383e7797eedf21e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-04-05 09:41:55 +0200

    klik systeem werkt  bij 2 word element verwijderd

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,22 @@
 let instellingen_tag = document.getElementById('instellingen');
-let c = document.getElementsByClassName("instelling");
 let count_instelligen  = 0;
-
+let instelligen;
+let instelligen_inhoud;
+let node_instellignen;
 
 
 instellingen_tag.addEventListener('click', () => {
     count_instelligen += 1;
     if (count_instelligen == 1) {
-        let instelligen = document.createElement("div");
-        let instelligen_inhoud = document.createTextNode("Hallo wereld");
+        instelligen = document.createElement("div");
+        instelligen_inhoud = document.createTextNode("Hallo wereld");
         instelligen.style = "background-color: gray; height: 250px;";
         instelligen.appendChild(instelligen_inhoud);
     
-        let node_instellignen = document.getElementById("instellingnode");
+        node_instellignen = document.getElementById("instellingnode");
         node_instellignen.appendChild(instelligen);
     } else {
-        console.log(count_instelligen)
+        instelligen.parentNode.removeChild(instelligen)
+        count_instelligen = 0;
     }
 })
