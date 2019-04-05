a47f2eda20e2bda6193f8927ed58bb450dfdafe6
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-04-05 09:28:11 +0200

    div heeft kleur

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,8 @@ instellingen_tag.addEventListener('click', () => {
     let instelligen_inhoud = document.createTextNode("Hallo wereld");
     instelligen.appendChild(instelligen_inhoud);
 
+    instelligen.style = "background-color: gray; height: 250px;";
+
     let node_instellignen = document.getElementById("instellingnode");
     node_instellignen.appendChild(instelligen);
 })
