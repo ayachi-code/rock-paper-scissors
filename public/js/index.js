be86b46c98c691b2c57b480cac58d19182c4a158
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-04-05 09:30:50 +0200

    count systeem gemaakt dat als 1 keer geklikt word dan word de div gemaakt

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,20 @@
 let instellingen_tag = document.getElementById('instellingen');
 let c = document.getElementsByClassName("instelling");
+let count_instelligen  = 0;
+
 
 
 instellingen_tag.addEventListener('click', () => {
-    console.log("open instelligen")
-    let instelligen = document.createElement("div");
-    let instelligen_inhoud = document.createTextNode("Hallo wereld");
-    instelligen.appendChild(instelligen_inhoud);
-
-    instelligen.style = "background-color: gray; height: 250px;";
-
-    let node_instellignen = document.getElementById("instellingnode");
-    node_instellignen.appendChild(instelligen);
+    count_instelligen += 1;
+    if (count_instelligen == 1) {
+        let instelligen = document.createElement("div");
+        let instelligen_inhoud = document.createTextNode("Hallo wereld");
+        instelligen.style = "background-color: gray; height: 250px;";
+        instelligen.appendChild(instelligen_inhoud);
+    
+        let node_instellignen = document.getElementById("instellingnode");
+        node_instellignen.appendChild(instelligen);
+    } else {
+        console.log(count_instelligen)
+    }
 })
